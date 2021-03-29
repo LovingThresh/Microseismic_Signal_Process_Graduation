ffef2056319a01a34f59709a3a2a307d2e2a21f4
liuye <csu1704liuye@163.com>
2021-03-29 23:43:36 +0800

    增加了VGG16模型 增加了Plot_result函数

---
+++ function/Plot_result.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+
+def plot(history):
+    """
+    打印结果
+    :param history:
+    :return:
+    """
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = [i for i in range(1, len(loss) + 1)]
+
+    plt.figure(1)
+
+    plt.plot(epochs, loss, 'bo', label="Training loss")
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
+
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    epochs = [i for i in range(1, len(acc) + 1)]
+
+    plt.figure(2)
+
+    plt.plot(epochs, acc, 'bo', label="Training accuracy")
+    plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.show()
